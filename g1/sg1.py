0a57a14eadb3b521256913a6f3d8f66cea267568
TahaaFatima <tahafatima95@gmail.com>
2020-02-10 16:51:51 +0500

    UI functionality for the given functions

--- g1/sg1.py
+++ g1/sg1.py
@@ -1,8 +1,8 @@
-def add(num1,num2):
+def Add(num1,num2):
     sum = num1 + num2
     return sum
 
-def subtract(num1,num2):
+def Subtract(num1,num2):
     if num1 > num2:
         sub = num1 - num2
         return sub
--- g1/sg2.py
+++ g1/sg2.py
@@ -1,1 +1,7 @@
-print("Maria-Ahmed")
+import sg1
+
+user_num_one = float(input("Enter a number : "))
+user_num_two = float(input("Enter a number : "))
+
+print(f'The answer of addition of the two numbers : {sg1.Add(user_num_one, user_num_two)}')
+print(f'The answer of subtraction of the two numbers : {sg1.Subtract(user_num_one, user_num_two)}')
