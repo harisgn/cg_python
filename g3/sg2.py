2e2381949489f036e8741aa67992f6342cc7feaa
fatima-777 <fzehra777@gmail.com>
2020-02-10 16:58:10 +0500

    UI code for g3

--- g3/sg2.py
+++ g3/sg2.py
@@ -1,1 +1,12 @@
-print("Maria-Ahmed")
+import sg1
+
+print(f"Please Enter 2 numbers for addition:")
+num1=int(input())
+num2=int(input())
+
+result1=sg1.Add(num1,num2)
+
+result2=sg1.Sub(num1,num2)
+
+print(f"The result of Addition is: {result1}")
+print(f"The result of Subtraction is: {result2}")
