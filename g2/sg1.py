3e6e270ae6fc0e08c1aa13fc5eeac98ea74c77c1
hudamerchant <hudazehra2510@gmail.com>
2020-02-10 16:50:00 +0500

    Completed

--- g2/sg1.py
+++ g2/sg1.py
@@ -1,16 +1,8 @@
 def Add(num1,num2):
-    a = num1
-    b = num2
     sum = num1 + num2
     return sum
     
-    
-
-
-
 def Sub(num1,num2):
-    x = num1
-    y = num2
     subtract  = num1 - num2
     return subtract
     
--- g2/sg2.py
+++ g2/sg2.py
@@ -1,1 +1,16 @@
-print("Maria-Ahmed")
+import sg1
+
+num1        = float(input("Enter first num :"))
+num2        = float(input("Enter second num :"))
+print('Select Operator')
+operator    = input("Add or Sub :")
+
+
+if operator.lower() == 'add':
+    result = sg1.Add(num1, num2)
+    print(f"Answer  = {result}")
+
+elif operator.lower() == 'sub':
+    result = sg1.Sub(num1, num2)
+    print(f"Answer  = {result}")
+ 
