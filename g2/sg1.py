6d9f9851f44dc35677fdd68f902c36b1fad7a1c8
Iffat Tehseen <iffat.tehseen1109@gmail.com>
2020-02-10 16:45:16 +0500

    sg1.py added

--- g2/sg1.py
+++ g2/sg1.py
@@ -1,1 +1,16 @@
-print("Maria-Ahmed")
+def Add(num1,num2):
+    a = num1
+    b = num2
+    sum = num1 + num2
+    return sum
+    
+    
+
+
+
+def Sub(num1,num2):
+    x = num1
+    y = num2
+    subtract  = num1 - num2
+    return subtract
+    
