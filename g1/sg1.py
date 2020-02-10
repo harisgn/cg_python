7ce27f5ca9d84228357236cf009bfdf145ad4003
ridarajani <ridarajani125@gmail.com>
2020-02-10 16:44:06 +0500

    group 1

--- g1/sg1.py
+++ g1/sg1.py
@@ -1,1 +1,11 @@
-print("Maria-Ahmed")
+def add(num1,num2):
+    sum = num1 + num2
+    return sum
+
+def subtract(num1,num2):
+    if num1 > num2:
+        sub = num1 - num2
+        return sub
+    elif num1 < num2:
+        sub = num2 - num1
+        return sub
